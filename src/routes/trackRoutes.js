8f72192529dbbfffe7def799b8d754b263d55df3
Ahmad Luqman <ahmad-luqman@users.noreply.github.com>
2022-11-18 12:07:10 +0500

    create get track by id routes

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -14,4 +14,30 @@ router.get('/tracks', async (req, res) => {
   res.send(tracks);
 });
 
+router.get('/tracks/:id', async (req, res) => {
+  const track = await Track.findById(req.params.id);
+
+  res.send(track);
+});
+
+router.post('/tracks', async (req, res) => {
+  const { name, locations } = req.body;
+  console.log(req);
+  console.log(req.body);
+
+  if (!name || !locations) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide a name and locations' });
+  }
+
+  try {
+    const track = new Track({ name, locations, userId: req.user._id });
+    await track.save();
+    res.send(track);
+  } catch (err) {
+    res.status(422).send({ error: err.message });
+  }
+});
+
 module.exports = router;
